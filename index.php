8c54dd9dfb209f2482f4af8a53f92ccb02023c48
Виктория <nikitina.post@gmail.com>
2021-12-28 08:19:14 +0300

    Добавлен вывод переменной с именем

--- index.php
+++ index.php
@@ -68,7 +68,7 @@ $user_name = 'Никитина Виктория'; // укажите здесь �
                             </div>
                             <div class="header__profile-name">
                                 <span>
-                                    <!--здесь должно быть имя пользователя-->
+                                   <?php print ($user_name); ?> 
                                 </span>
                                 <svg class="header__link-arrow" width="10" height="6">
                                     <use xlink:href="#icon-arrow-right-ad"></use>
