e3995003cbb55c249aa79582e65681745f46389a
Виктория <nikitina.post@gmail.com>
2022-04-05 21:17:05 +0300

    Функция

--- index.php
+++ index.php
@@ -1,4 +1,6 @@
 <?php
+mb_internal_encoding("UTF-8");
+
 $isAuth = rand(0, 1);
 
 $userName = 'Никитина Виктория'; // укажите здесь ваше имя
@@ -48,26 +50,13 @@ function cutText ($text, $numLetters = 300) {
     if($textNum > $numLetters) {
         
         $words = explode(" ", $text);
-        $text = [];
         
-        foreach ($words as $word) {  
-            
-            $num = mb_strlen($word);
-            $numSum += $num;
-            
-            if ($numSum <= $numLetters) {
-                $text[] = $word; 
-            } else {
-                break;
-            }
-        }
+        $arrWords = array_slice($words, 0, $numLetters);  
         
-        $text = implode(" ", $text);
+        $text = implode(" ", $arrWords);
         $text .= '... <a class="post-text__more-link" href="#">Читать далее</a>';
         
-    } else {
-        $text;
-    }
+    } 
     
     return $text;
 }
