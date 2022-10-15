241afb4d580e4655fe77350b81c0cd8623ac05c3
Виктория <nikitina.post@gmail.com>
2022-10-15 13:41:42 +0300

    Внесены правки и написана новая функция

---
+++ functions.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Обрезает текстовое содержимое если оно превышает заданное число символов. Также, если текст был обрезан, добавляет к нему ссылку «Читать далее»
+ *
+ * @param string $text Данные для обрезания
+ * @param int $numLetters Количество символов, до которого нужно обрезать текст 
+ *
+ * @return string
+ */
+function cut_text ($text, $numLetters = 300) {
+
+    $textNum = mb_strlen($text);
+
+    if($textNum > $numLetters) {
+        
+        $text = mb_substr($text, 0, $numLetters,'UTF-8'); // Обрезаем и работаем со всеми кодировками и указываем исходную кодировку
+        $position = mb_strrpos($text, ' ', 'UTF-8'); // Определение позиции последнего пробела. Именно по нему и разделяем слова
+        $text = mb_substr($text, 0, $position, 'UTF-8'); // Обрезаем переменную по позиции
+
+        $text .= '... <a class="post-text__more-link" href="#">Читать далее</a>';
+
+    } 
+
+    return $text;
+}
