f9cb6d5b1425de51499c6bd462ea5b16424bbf96
akabirov <akabirov2@gmail.com>
2023-05-24 16:05:21 +0300

    Update index.php
    Для отображения строки

--- index.php
+++ index.php
@@ -123,7 +123,7 @@ $posts = get_result($db_link, $query, 2);
 // Генерация дат
 
 foreach ($posts as $key => $post) {
-    $posts[$key]['date_title'] = date("d.m.Y H:i", strtotime($post['dt_add']));
+    $posts[$key]['date_title'] = date("d.m.Y H:i", strtotime($post['dt_add'])); 
 
     $posts[$key]['date_interval'] = get_interval($post['dt_add']);
 }
