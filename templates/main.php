8b9806e196059904a8c850d134f7f5fa3492e62d
akabirov <akabirov2@gmail.com>
2023-05-24 17:07:47 +0300

    Update main.php
    показ строки

--- templates/main.php
+++ templates/main.php
@@ -104,7 +104,7 @@
                     	<a class="post-link__external" href="http://<?= $post['p_content']; ?>" title="Перейти по ссылке">
                         	<div class="post-link__info-wrapper">
                             	<div class="post-link__icon-wrapper">
-                                	<img src="https://www.google.com/s2/favicons?domain=<?= $post['p_content']; ?>" alt="Иконка">
+                                	<img src="https://www.google.com/s2/favicons?domain=<?= $post['p_content']; ?>" alt="Иконка">  
                                 </div>
                                 <div class="post-link__info">
                                 	<h3><?= $post['p_title']; ?></h3>
