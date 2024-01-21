5b9618669dd0c3ab5bd9e0194a7b071f0ea83602
Виктория <nikitina.post@gmail.com>
2024-01-21 18:30:05 +0300

    $form_tags создано с использованием include_template() и простого шаблона

--- add.php
+++ add.php
@@ -159,12 +159,9 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     }
 }
 
-$form_tags = '<div class="adding-post__input-wrapper form__input-wrapper">
-                <label class="adding-post__label form__label" for="tags">Теги</label>
-                <div class="form__input-section">
-                    <input class="adding-post__input form__input" id="tags" type="text" name="tags" placeholder="Введите теги" value="">
-                </div>
-              </div>';
+$form_tags = include_template('form-tags.php', [
+    'tags' => $post_data['tags']
+]);
 
 $form_invalid_block = '';
 $form_invalid_block_items = '';
